31413a061c3c36d185855aa58702493e5e11ce66
0r10n <andrew.yurchenko@depositphotos.com>
2021-01-24 22:37:04 +0200

    Ping command and handler

--- src/Application/CQRS/Query/Ping.php
+++ src/Application/CQRS/Query/Ping.php
@@ -5,8 +5,17 @@ declare(strict_types=1);
 namespace OrionUa\DDDCore\Application\CQRS\Query;
 
 use OrionUa\DDDCore\Application\CQRS\QueryInterface;
+use OrionUa\DDDCore\SerializationAwareInterface;
 
-class Ping implements QueryInterface
+class Ping implements QueryInterface, SerializationAwareInterface
 {
+    public static function newFromArray(array $data)
+    {
+        return new self();
+    }
 
+    public function jsonSerialize()
+    {
+        return [];
+    }
 }
